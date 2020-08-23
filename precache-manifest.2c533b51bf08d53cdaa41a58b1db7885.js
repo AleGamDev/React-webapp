10e252fea21ca6e79baa4bc47cf9434cda0fd973
Ale Gamero <Mastercadena@hotmail.com>
2020-08-23 17:57:59 +0200

    Updates

---
+++ precache-manifest.2c533b51bf08d53cdaa41a58b1db7885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8206da5d58556f43487c1f1b4c4044e9",
+    "url": "/React-webapp/index.html"
+  },
+  {
+    "revision": "b6e1116a9f9c31821d72",
+    "url": "/React-webapp/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "70e3093a4e43f5665839",
+    "url": "/React-webapp/static/css/main.64b6512b.chunk.css"
+  },
+  {
+    "revision": "b6e1116a9f9c31821d72",
+    "url": "/React-webapp/static/js/2.1aa83a8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-webapp/static/js/2.1aa83a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e3093a4e43f5665839",
+    "url": "/React-webapp/static/js/main.162d0a5b.chunk.js"
+  },
+  {
+    "revision": "e288f855c71afbea4e29",
+    "url": "/React-webapp/static/js/runtime-main.5d4ff060.js"
+  }
+]);
